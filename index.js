d7d15d429fa74d891b4bc29bd12170e4d2c3f48a
Alina Ishchenko <alinaish@gmail.com>
2018-09-13 22:18:10 +0100

    parse vk audio html

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const fs = require('fs');
+const htmlParser = require('node-html-parser');
+
+const vkHtmlPath = process.argv[2];
+
+if (!vkHtmlPath) {
+  return;
+}
+
+function getVKPlaylistFromHtml() {
+  fs.readFile(vkHtmlPath, 'utf8', (err, htmlContent) => {
+    if (err) {
+      throw err;
+    }
+
+    const html = htmlParser.parse(htmlContent);
+    const vkPlaylist = html
+      .querySelectorAll('.audio_row__inner')
+      .map(audioRow => {
+        return {
+          artist: audioRow.querySelector('.artist_link').text,
+          track: audioRow.querySelector('._audio_row__title_inner').text,
+        };
+      });
+    console.log(vkPlaylist);
+  });
+}
+
+getVKPlaylistFromHtml();
