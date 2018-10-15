b4bb40c25104c2174725404d5a3f87ee6693632c
Alina Ishchenko <alinaish@gmail.com>
2018-10-15 22:50:10 +0100

    setup redirect for spotify auth

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const htmlParser = require('node-html-parser');
-
-const vkHtmlPath = process.argv[2];
-
-if (!vkHtmlPath) {
-  return;
-}
+const http = require('http');
 
 function getVKPlaylistFromHtml() {
+  const vkHtmlPath = process.argv[2];
+
+  if (!vkHtmlPath) {
+    return;
+  }
+
   fs.readFile(vkHtmlPath, 'utf8', (err, htmlContent) => {
     if (err) {
       throw err;
@@ -26,4 +27,28 @@ function getVKPlaylistFromHtml() {
   });
 }
 
-getVKPlaylistFromHtml();
+// getVKPlaylistFromHtml();
+
+/* Create an HTTP server to handle responses */
+http
+  .createServer(function(request, response) {
+    const url = request.url;
+
+    switch (url) {
+      case '/about':
+        const clientId = '2d53491834a9437eb3b5ccb7cdce4832';
+        const scope = encodeURIComponent('playlist-modify-private');
+        const redirectURI = encodeURIComponent('http://localhost:8888');
+        const spotifyAuthUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectURI}`;
+        response.writeHead(301, {
+          'Content-Type': 'text/plain',
+          Location: spotifyAuthUrl,
+        });
+        break;
+      default:
+        response.writeHead(200);
+        response.write('Hello World');
+    }
+    response.end();
+  })
+  .listen(8888);
